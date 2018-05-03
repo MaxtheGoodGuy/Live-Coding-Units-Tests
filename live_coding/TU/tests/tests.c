e392a9f5183555dc113094988ad4adc5fddd7647
MaxtheGoodGuy <maximilien.oteifeh-pfennig@epitech.eu>
2018-05-03 17:15:52 +0200

    Testing git hook (pre-commit)

--- live_coding/TU/tests/tests.c
+++ live_coding/TU/tests/tests.c
@@ -5,7 +5,7 @@
 ** Login   <maximilien.oteifeh-pfennig@epitech.eu>
 ** 
 ** Started on  Wed Apr 11 10:46:32 2018 MextheGoodGuy
-** Last update Thu May  3 16:04:50 2018 MextheGoodGuy
+** Last update Thu May  3 17:15:35 2018 MextheGoodGuy
 */
 
 #include <string.h>
@@ -14,7 +14,7 @@
 
 Test(my_strlen, return_value)
 {
-  cr_assert_eq(my_strlen("lel"), 2);
+  cr_assert_eq(my_strlen("lel"), 3);
 }
 
 Test(my_strlen, strlen)
